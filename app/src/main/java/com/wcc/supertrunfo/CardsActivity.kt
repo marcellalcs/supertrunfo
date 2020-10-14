0dca7e163e86f0cebf195b68cdc506fa4ab44277
Marcella Souza <mlsouza@thoughtworks.com>
2020-10-14 10:02:56 -0300

    Adding first game screen

---
+++ app/src/main/java/com/wcc/supertrunfo/CardsActivity.kt
@@ -0,0 +1,11 @@
+package com.wcc.supertrunfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cards)
+    }
+}
